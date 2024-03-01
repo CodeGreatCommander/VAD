729098d8b84be0ed90b689e4e6f3f5ace53eff20
CodeGreatCommander <rohcoding@gmail.com>
2024-03-01 09:50:33 +0530

    Supports silero as well

--- inference/pretrained_models/header_files/proximityFunction.h
+++ inference/pretrained_models/header_files/proximityFunction.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <cmath>
-double normal(double x, double y, double scale = 1.0);
+double normal(double x, double y, double scale = 1);
 double uniform(double x, double y, double scale = 1e9);
 double weibull(double x, double y, double scale = 1.0, double shape = 1.0);
+double checkbox(double x, double y, double scale = 1.0);
+double beta(double x, double y, double scale = 1.0, double shape1 = 1.0, double shape2 = 1.0);
--- inference/pretrained_models/inference/proximityFunction.cpp
+++ inference/pretrained_models/inference/proximityFunction.cpp
@@ -21,3 +21,13 @@ double weibull(double x, double y, double scale, double shape) {
     double result = std::exp(-std::pow(distance / scale, shape));
     return result;
 }
+
+double checkbox(double x, double y, double scale) {
+    return 1e6;
+}
+
+double beta(double x, double y, double scale, double shape1, double shape2) {
+    double distance = std::abs(x - y);
+    double result = std::pow(distance, shape1 - 1) * std::pow(scale - distance, shape2 - 1);
+    return result;
+}
